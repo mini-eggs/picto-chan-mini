428ef36d4e8f0f6a11247703de40a9a996af8547
Evan M Jones <evanjones4040@gmail.com>
2019-07-07 20:09:39 +0000

    Fix(main.go): For some reason the Go file lost some formatting.

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ type data struct {
 	Source  string `json:"-"`
 	IsImage bool   `json:"-"`
 }
+
 type list struct {
 	Posts []data `json:"posts"`
 }
@@ -28,6 +29,7 @@ func H(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router().Run()
 }
+
 func router() *gin.Engine {
 	r := gin.Default()
 	r.LoadHTMLGlob("./templates/*")
@@ -36,30 +38,36 @@ func router() *gin.Engine {
 	r.NoRoute(serveNotFound)
 	return r
 }
+
 func serveNotFound(c *gin.Context) {
 	c.HTML(http.StatusOK, "main.html", gin.H{})
 }
+
 func serveThread(c *gin.Context) {
 	board := c.Param("board")
 	thread := c.Param("thread")
 	url := fmt.Sprintf("https://a.4cdn.org/%s/thread/%s.json", board, thread)
+
 	res, err := http.Get(url)
 	if err != nil {
 		c.String(http.StatusBadGateway, err.Error())
 		return
 	}
 	defer res.Body.Close()
+
 	b, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		c.String(http.StatusBadGateway, err.Error())
 		return
 	}
+
 	var all list
 	err = json.Unmarshal(b, &all)
 	if err != nil {
 		c.String(http.StatusBadGateway, err.Error())
 		return
 	}
+
 	var media list
 	for _, val := range all.Posts {
 		img := isImage(val.Ext)
@@ -69,12 +77,15 @@ func serveThread(c *gin.Context) {
 			media.Posts = append(media.Posts, data{Source: source, IsImage: img})
 		}
 	}
+
 	id := fmt.Sprintf("ID-%s-%s", board, thread)
 	c.HTML(http.StatusOK, "thread.html", gin.H{"List": media, "ID": id})
 }
+
 func isImage(ext string) bool {
 	return ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".gif"
 }
+
 func isVideo(ext string) bool {
 	return ext == ".webm"
 }
